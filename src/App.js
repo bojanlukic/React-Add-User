d57efca39a9f3cd283fd2830d0023d7964d81b53
Bojan Lukić <bojan.lukic@givesense.rs>
2022-09-06 15:40:47 +0200

    Fifth Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import SearchBar from './components/SearchBar';
 import './../src/App.css'
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [employees, setEmployees] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/PERSON')
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => setEmployees(data))
       .catch(err => console.log('Greska pri ucitavanju URL-a'))
+    
   }, []);
 
  
@@ -19,26 +21,29 @@ function App() {
       <table align='center' className='table'>
         <thead>
           <tr>
+            <th>Id</th>
             <th>Name</th>
             <th>Surname</th>
             <th>UserType</th>
             <th>CreatedDate</th>
             <th>City</th>
-            <th>Adress</th>
+            <th>Adress</th>  
           </tr>
         </thead>
         <tbody>
+        {employees.map((employee)=> (
         
-            <tr>
-              <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          <tr key={employee.id}>
+            <td>{employee.id }</td>
+            <td>{employee.Name } </td>
+            <td>{ employee.Surname}</td>
+            <td>{ employee.UserType}</td>
+            <td>{employee.CreatedDate}</td>
+            <td>{employee.City }</td>
+            <td>{employee.Adress }</td>
             </tr>
-            
-        
+             
+             ))}
          
         </tbody>
       </table>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 
 function SearchBar() {
+  const [text, setText] = useState('');
+
+  const submitHandler= (e) => {
+    e.preventDefault();
+
+  }
+
   return (
+
     <div className="input">
-      
+     <form onSubmit={submitHandler}> 
       <label>Name</label>
-      <input type="text" />
+      <input onChange={(e)=>(setText(e.target.value))} type="text" />
       <label>User Type</label>
-      <select>
-        <option>Praksa</option>
-        <option>Zaposlen</option>
-        <option>Nezaposlen</option>
+      <select onChange={(e) => (setText(e.target.value))}>
+        <option>Internship</option>
+        <option>Employed</option>
+        <option>Unemployed</option>
       </select>
-        <button>Search</button>
-          
+        <button type="submit">Search</button>
+        </form>
     </div>
   );
 }
